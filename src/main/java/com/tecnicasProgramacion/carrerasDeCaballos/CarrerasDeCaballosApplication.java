6a301de4d8e5452dfbe5f9e81a1a69b72809f8e2
lucas.martinez2 <lucas.martinez@naranjax.com>
2024-08-24 16:32:08 -0300

    Inicializo proyecto springboot y creo controller hello world

---
+++ src/main/java/com/tecnicasProgramacion/carrerasDeCaballos/CarrerasDeCaballosApplication.java
@@ -0,0 +1,13 @@
+package com.tecnicasProgramacion.carrerasDeCaballos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrerasDeCaballosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrerasDeCaballosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tecnicasProgramacion/carrerasDeCaballos/CarrerasDeCaballosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecnicasProgramacion.carrerasDeCaballos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrerasDeCaballosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
